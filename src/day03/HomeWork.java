2a161528b843f4e4d2c7ee208eff3331882faf99
whh996 <1873652770@qq.com>
2020-06-17 08:47:42 +0800

    问海红作业第一次项目测试提交

---
+++ src/day03/HomeWork.java
@@ -0,0 +1,15 @@
+package day03;
+
+/**
+ * @Author: whh
+ * Description:
+ * @Data:Create in 8:44 2020/6/17
+ * Nodified By:
+ */
+public class HomeWork {
+    public static void main(String[] args) {
+        //打印99乘法表
+        System.out.println("等腰三角形");
+        //打印等腰三角形
+    }
+}
